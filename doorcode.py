66a18839d09a43d15451dd0a534e4279fa2df55b
VoHTB <orsinov@proton.me>
2024-08-26 23:06:47 +0100

    Shitting itself itself at duplicates is better than... just shitting itself

--- doorcode.py
+++ doorcode.py
@@ -71,6 +71,10 @@ def location_selection():
 		# The user now chooses one
 		choice = input("Select a location: ")
 
+		# Just came back from dental surgery.
+		#   Have never wanted donner more than right now.
+		#    - S
+
 		try:
 			# File name inputted
 			if choice in locations_dir:
@@ -94,7 +98,74 @@ def location_selection():
 	location_human_name = locations_human_names[int(location_index)]
 	# Notify the user
 	print(f"{Colour.GREEN}{location_human_name}{Colour.END} (at '{location}') selected.")
-	return location
+	return f"./locations/{location}"
 
-#DEBUG
-location_selection()
+
+# SEARCH FOR A ROOM
+# Load the location information and find the target
+def search(location, target):
+	# Make sure the location you want to access does, in fact, exist
+	assert os.path.exists(location), f"File ('{location}') does not exist."
+
+	# Get the codes from the file at specified location
+	file_contents = open(location, "r").readlines()
+	# Get some data to do some end-user stuff
+	human_name = file_contents[0]
+
+
+	# Find the target
+	rooms = []
+	room_type = "UNKNOWN:"
+	exact_match = False
+	for room in file_contents:
+		# Make a raw copy of the record
+		raw_room = room
+		# Split the record into managable chunks
+		room = room[:-1].split(",")
+
+		# Check! Is this record a label?
+		if len(room) == 1 and room[0][-1] == ":":
+			# If it is, update the room type
+			room_type = room[0]
+			# This is a new section to the file, find another exact match
+			exact_match = False
+			# Skip to the next record: it can't be a label and a room code!
+			continue
+
+		# I sit in my room listening to 'Wait' by C418...
+		#  It is dark and the light is calming...
+		#   Why must all the best feelings come at 22:23?
+		#    - S
+
+		# If its a copy, don't bother
+		if [room_type, room] in rooms:
+			continue
+
+		# If an exact match hasn't been found yet,
+		#  and the target is a substring of this room,
+		#   and it's not entierly a duplicate
+		if not exact_match and target in room[0]:
+			# If the 'exact' match has been found
+			if target == room[0]:
+
+				# Kill all rooms with this type that don't match exactly
+				dead_rooms = []
+				# Collect all the rooms which aren't exact
+				for dead_room in rooms:
+					# If the room type is a copy, 
+					if dead_room[0] == room_type:
+						dead_rooms.append(dead_room)
+				# Remove them all from the rooms
+				for dead_room in dead_rooms:
+					rooms.remove(dead_room)
+
+				# We have found an exact match
+				#  Don't stop until a new type is found
+				exact_match = True
+
+			# Add the room to the results list
+			rooms.append([room_type, room])
+
+	print(rooms)
+
+search(location_selection(), input("> "))
